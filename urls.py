ebde0aac6b10cb4a1d2994ae366ec4a60198a804
Ton <ton@jarrbo.nl>
2023-08-29 19:17:32 +0200

    replace from django.conf.urls import url with from django.urls import re_path replace url with re_path

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.urls import re_path
 from django.contrib import admin
 from .views import jarrbo_home, ContactView, ThanksView, ProfileView
 
 urlpatterns = [
-    url(r'^$', jarrbo_home, name='jarrbo_home'),
-    url(r'^contact/$', ContactView, name='contact'),
-    url(r'^contact/thanks', ThanksView, name='thanks'),
-    url(r'^profile/$', ProfileView, name='profile'),
+    re_path(r'^$', jarrbo_home, name='jarrbo_home'),
+    re_path(r'^contact/$', ContactView, name='contact'),
+    re_path(r'^contact/thanks', ThanksView, name='thanks'),
+    re_path(r'^profile/$', ProfileView, name='profile'),
 ]
